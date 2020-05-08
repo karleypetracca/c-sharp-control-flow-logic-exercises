e10de0282c375cad0adcb97b4b925a7de49f2447
Karley Petracca <karleypetracca@gmail.com>
2020-05-08 15:00:22 -0400

    Uncomment first exercise to guarantee build run and small change to console message for user input

--- Program.cs
+++ Program.cs
@@ -7,26 +7,26 @@ namespace ControlFlowExercises1
   class Program
   {
 
-    // /*
-    //  Write a program and ask the user to enter a number.The number should be
-    //  between 1 to 10.If the user enters a valid number, display "Valid" on the
-    //  console.Otherwise, display "Invalid". (This logic is used a lot in
-    //  applications where values entered into input boxes need to be validated.)
-    //  */
-    // static void Main(string[] args)
-    // {
-    //   Console.Write("Please enter a number: ");
-    //   var number = Convert.ToInt32(Console.ReadLine());
-    //
-    //   if (number >= 1 && number <= 10)
-    //   {
-    //     Console.WriteLine("Valid");
-    //   }
-    //   else
-    //   {
-    //     Console.WriteLine("Invalid");
-    //   }
-    // }
+    /*
+     Write a program and ask the user to enter a number.The number should be
+     between 1 to 10.If the user enters a valid number, display "Valid" on the
+     console.Otherwise, display "Invalid". (This logic is used a lot in
+     applications where values entered into input boxes need to be validated.)
+     */
+    static void Main(string[] args)
+    {
+      Console.Write("Please enter a number between 1 and 10: ");
+      var number = Convert.ToInt32(Console.ReadLine());
+
+      if (number >= 1 && number <= 10)
+      {
+        Console.WriteLine("Valid");
+      }
+      else
+      {
+        Console.WriteLine("Invalid");
+      }
+    }
 
 
     // /*
